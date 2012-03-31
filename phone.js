0fc281961f5d93c88d077c070ed556802ff963f4
Francis Gulotta <wizard@roborooter.com>
2012-03-31 18:17:51 -0400

    add config loading for twilio

--- phone.js
+++ phone.js
@@ -1,4 +1,14 @@
 var util = require('util');
+var nconf = require('nconf');
+
+var twilioConf = nconf.env().argv().file({file: './config.json'}).get('twilio');
+
+if(!twilioConf || ! twilioConf.sid || !twilioConf.token){
+  console.error( util.inspect(twilioConf));
+  throw new Error('Twilio settings are not set!');
+}
+
+
 
 var parser = function(req,res,next){
   req.body || next();
