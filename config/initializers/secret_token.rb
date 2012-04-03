afe91d7c067821f9aba3309653bae6f3364c7f64
Francis Gulotta <wizard@roborooter.com>
2012-04-02 22:50:20 -0400

    add rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sendmycontactinfo::Application.config.secret_token = 'b7a240acdb40814b140540fecf0e67398ed17857e95b5ecbf9c992ad78c915ff7aafc3303de8d4e5e371ceb7d9d55e8cfaa052d5fa5c4122b7d87058d5da8c52'
